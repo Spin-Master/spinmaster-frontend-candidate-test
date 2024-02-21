c69d0ec043b9d4f3f1c40a77e5369123fbf6f525
Valerie Wireko Osei <valeriew@SML391TO.local>
2024-02-21 17:35:18 -0500

    initial commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,17 @@
+/*
+Frontend React Coding Challenge:
+
+Refactor the hardcoded data below, so that you are fetching the data from https://jsonplaceholder.typicode.com/photos?limit=50
+
+Using responsive design best practices, build a listing page that is suitable on desktop and mobile devices; the image on the cards should use the thumbnail URL and the layout should collapse to a single column on mobile. 
+
+Although CSS is not a huge focus for this exercise, please ensure to make use of the required CSS to create a basic responsive grid layout.
+
+## Stretch Challenge:
+
+Once you have your layout complete, a user should be able to click on a card thumbnail, and the image url should pop up on click.
+*/
+
 import './App.css';
 
 
